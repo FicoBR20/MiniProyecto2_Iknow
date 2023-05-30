4dad7965c48beced108e77ed82ae1452b82d3aa5
Jose Erley Murillo <jerley@live.com>
2023-05-30 11:16:50 -0500

    Creación de el método que me permite leer una lista desde un archivo de texto

--- src/modelo/Palabra.java
+++ src/modelo/Palabra.java
@@ -1,8 +1,17 @@
 package modelo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class Palabra {
     private String palabra_a_Memorizar;
     private Boolean acierto;
+    private  List<String> Lista_palabra_a_Memorizar;
 
     public String getPalabra_a_Memorizar() {
         return palabra_a_Memorizar;
@@ -20,13 +29,72 @@ public class Palabra {
         this.acierto = acierto;
     }
 
+
     /**
      * Método constructor
      */
     public Palabra(){
+        ini();
         palabra_a_Memorizar = " ";
         acierto = false;
     }
+
+
+    //Crea un método para leer el archivo de texto y obtener las palabras
+    public List<String> obtenerPalabrasAleatorias(String archivo) throws IOException {
+        Lista_palabra_a_Memorizar = new ArrayList<>();
+
+        BufferedReader lector = new BufferedReader(new FileReader(archivo));
+        String linea;
+
+        while (lector.readLine() != null){
+            linea = lector.readLine();
+            linea.split("\n");
+            //Recorre cada letra de una palabra
+            for (String palabra : linea.split("\n")) {
+                // Remplaza cualquier caracter que no sea alfabetico con un espacio vacio
+                palabra = palabra.replaceAll("^[^a-zA-Z]+|[^a-zA-Z]+$", "");
+                if (!linea.isEmpty()) {
+                    Lista_palabra_a_Memorizar.add(palabra);
+                }
+            }
+        }
+
+        lector.close();
+
+        return Lista_palabra_a_Memorizar;
+    }
+
+    //Crea un método para seleccionar una palabra aleatoria del arreglo
+    public String seleccionarPalabraAleatoria(List<String> palabras) {
+        Random random = new Random();
+        int indiceAleatorio = random.nextInt(palabras.size());
+        return palabras.get(indiceAleatorio);
+    }
+
+    //Usa los métodos de obtencion y seleccion a modo de prueba
+    public void ini(){
+        try {
+            String archivo = "src/modelo/animales.txt";
+            List<String> palabras = obtenerPalabrasAleatorias(archivo);
+
+            if (!palabras.isEmpty()) {
+                String palabraAleatoria = seleccionarPalabraAleatoria(palabras);
+                System.out.println("Palabra aleatoria: " + palabraAleatoria);
+            } else {
+                System.out.println("No se encontraron palabras en el archivo.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Palabra palabra = new Palabra();
+    }
+
+
+
 
     /**
      * Método que retorna true si el string ingresado es idéntico
